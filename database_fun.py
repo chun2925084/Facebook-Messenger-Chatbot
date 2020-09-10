591148b6f928e8add5e45eef82f30784dd4572ac
chun2925084 <chun2925084@gmail.com>
2020-09-10 16:35:17 +0800

    Create Heroku database

---
+++ database_fun.py
@@ -0,0 +1,20 @@
+import os
+import psycopg2
+
+
+def create_database:
+    DATABASE_URL = os.environ['DATABASE_URL']
+
+    conn = psycopg2.connect(DATABASE_URL, sslmode='require')  # Connect the Heroku database
+
+    cursor = conn.cursor()
+
+            SQL_order = '''CREATE TABLE account(
+                            subject, class, url, path, name, finish, solution
+                    );'''
+        
+            cursor.execute(SQL_order)
+            conn.commit()
+
+            cursor.close()
+            conn.close()
