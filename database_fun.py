a42f18eab3151997db1ee012bf742cd32465c212
chun2925084 <chun2925084@gmail.com>
2020-09-11 14:26:28 +0800

    Fix database syntax error

--- database_fun.py
+++ database_fun.py
@@ -2,7 +2,7 @@ import os
 import psycopg2
 
 
-def create_database:
+def create_database():
     DATABASE_URL = os.environ['DATABASE_URL']
 
     conn = psycopg2.connect(DATABASE_URL, sslmode='require')  # Connect the Heroku database
